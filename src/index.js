649b3e0102b64fa55b5e1aadbf46938300f13590
Nick <ex-er-sis@ya.ru>
2023-08-03 05:53:36 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-import App from './component.jsx';
+import App from './component';
 
-const root = document.getElementById('root');
-
-render(<App />, root);
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+// Try the environment variable, otherwise use root
+const ASSET_PATH = process.env.ASSET_PATH || '/public';
+
 module.exports = {
+  entry: './src/index.js', // Note that without this option webpack just try find entry in ./src/
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
   resolve: {
+    extensions: ['.js', '.jsx'], // For the correct resolve index files without specifying the extension
     modules: [path.join(__dirname, 'src'), 'node_modules'],
     alias: {
       react: path.join(__dirname, 'node_modules', 'react'),
     },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'), // For correct resolve files from public dir in dev mode
+    },
+    compress: true,
   },
   module: {
     rules: [
@@ -21,22 +32,27 @@ module.exports = {
           loader: 'babel-loader',
           options: { presets: [
             ['@babel/preset-env', { targets: "ie 9" }],
-            '@babel/preset-react'
+            ['@babel/preset-react', { "runtime": "automatic" }] // "runtime": "automatic" need to enable new jsx transform and do not import react
           ]}
         },
       },
       {
-        test: /\.css$/,
+        test: /\.(sa|sc|c)ss$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
+          { loader: 'style-loader' }, // Inject CSS into the DOM (Creates `style` nodes from JS strings)
+          { loader: 'css-loader' }, // Interprets @import and url() and will resolve them (Translates CSS into CommonJS)
+          { loader: 'sass-loader', // Compiles Sass to CSS
+            options: {
+              implementation: require("sass"), // Prefer `dart-sass`
+            },
+          }
         ],
       },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html',
+      template: './public/index.html',
     }),
   ],
 };
